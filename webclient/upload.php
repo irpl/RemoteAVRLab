6d14b82b3afae15069a017c39bcfaf07e663a7eb
irpl <philo3194@gmail.com>
2016-02-09 05:00:52 +0000

    accepts only .c, .hex and .asm files, for real this time

--- webclient/upload.php
+++ webclient/upload.php
@@ -4,12 +4,14 @@ $fileTmpLoc = $_FILES["file1"]["tmp_name"]; // File in the PHP tmp folder
 $fileType = $_FILES["file1"]["type"]; // The type of file it is
 $fileSize = $_FILES["file1"]["size"]; // File size in bytes
 $fileErrorMsg = $_FILES["file1"]["error"]; // 0 for false... and 1 for true
+$fileExtention = pathinfo($fileName, PATHINFO_EXTENSION);
 if (!$fileTmpLoc) { // if file not chosen
     echo "ERROR: Please browse for a file before clicking the upload button.";
     exit();
 }
-if($fileType != "c" || $fileType != "asm" || $fileType != "hex"){
+if($fileExtention != "c" && $fileExtention != "asm" && $fileExtention != "hex"){
     echo "Invalid file type. Please choose a .c, .asm or .hex file.";
+    echo $fileExtention;
     exit();
 }
 if(move_uploaded_file($fileTmpLoc, "test_uploads/$fileName")){
