8f1729bd797331164915e41c81fdd31feb049cbe
irpl <philo3194@gmail.com>
2016-03-05 01:19:26 -0500

    fixed login submit on enter key press

--- webclient/index.php
+++ webclient/index.php
@@ -21,13 +21,13 @@ if ($log_idnumber != ""){
     	    <form id="loginform" onsubmit="return false;">
     	    	<input id="idnumber" type="text" value="" placeholder="ID Number" onfocus="emptyElement('status')"/><br />
     	    	<input id="password" type="password" value="" placeholder="Password" onfocus="emptyElement('status')"/><br />
-    	    </form>
-    	    <p id="status"></p>
-    	    <div id="rem-reg">
-    	        <label><input type="checkbox" name="remember" value="remember" id="remember" /><span id="remember-text">Remember me</span></label>
-        	    <span id="not-reg"><a href="registration.html">Not yet registered?</a></span>
-    	    </div>
+    	    	<p id="status"></p>
+    	    	<div id="rem-reg">
+    	        	<label><input type="checkbox" name="remember" value="remember" id="remember" /><span id="remember-text">Remember me</span></label>
+        	    	<span id="not-reg"><a href="registration.html">Not yet registered?</a></span>
+    	    	</div>
     	    <button id="loginbtn" onclick="login()" >Login</button>
+    	    </form>
 	    </div>
 	</div>
 </body>
